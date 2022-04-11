94b3b6c8ab0786a73a9eaabccd6207cb7d34d761
Larry Rau <larryrau@mac.com>
2022-04-10 22:14:45 -0400

    add tls support; add generation of cert/key

--- lsrv.go
+++ lsrv.go
@@ -4,17 +4,18 @@
 // license that can be found in the LICENSE file.
 // code borrowed from:https://go.dev/doc/articles/wiki/final.go
 
-//go:build ignore
-
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+
+	"github.com/larryr/lsrv/lsrv"
 )
 
 type Page struct {
@@ -30,18 +31,57 @@ func init() {
 
 	templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 	validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 }
 
-func main() {
+const (
+	addr    = "0.0.0.0"
+	secCert = "cert.pem"
+	secKey  = "key.pem"
+)
 
+var (
+	notls   *bool   = flag.Bool("notls", false, "use unsecure http")
+	gencert *bool   = flag.Bool("gencert", false, "generate cert and key")
+	port    *int    = flag.Int("port", 8080, "port for server to listen on")
+	host    *string = flag.String("host", "localhost", "host name for certificate")
+)
+
+func main() {
+	flag.Parse()
+
+	var srvAddr = fmt.Sprintf("%s:%d", addr, *port)
+	fmt.Printf("lsrv: listening: %s\n", srvAddr)
+	log.Printf("args: notls=%v gencert=%v port=%v host=%v", *notls, *gencert, *port, *host)
+
+	if *gencert {
+		// generate certificate/key and exit.
+		err := lsrv.GenerateCert(*host, "")
+		if err != nil {
+			log.Fatalf("error generating cert:%v", err)
+		}
+		log.Printf("certificate/key generated!\n")
+		return
+	}
+
+	// setup to run server
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "content/"+r.URL.Path[1:])
 	})
-	fmt.Printf("lsrv: port=8080\n")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+
+	var err error
+	if *notls {
+		err = http.ListenAndServe(srvAddr, nil)
+	} else {
+		err = http.ListenAndServeTLS(srvAddr, secCert, secKey, nil)
+	}
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	log.Printf("exiting!\n")
 }
 
 func (p *Page) save() error {
